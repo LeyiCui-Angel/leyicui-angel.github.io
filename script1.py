ba679fbf71e079259eecd5243ba0e06797a3ccf6
[LeyiCui-Angel] <angelrawencraw@gmail.com>
2022-07-07 16:10:21 -0400

    Implement template
    Change to template in class

--- script1.py
+++ script1.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/leyicui-angel.github.io')
+@app.route('/')
 def home():
-    return "Here it is!"
+    return render_template("home.html")
 
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+
+# Case 1: Script executed
+# __name__ == "__main__"
+# Case 2: Script imported
+# __name__ == "script1"
 if __name__ == "__main__":
     app.run(debug=True)
